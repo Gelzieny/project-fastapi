fb6e1c8f632f4ade1144287b3d54e59c6161c091
Gelzieny <gelzieny@gmail.com>
2026-03-11 15:25:34 -0300

    fge

--- src/models/settings/database_connection_handler_teste.py
+++ src/models/settings/database_connection_handler_teste.py
@@ -4,6 +4,6 @@ from src.models.settings.database_connection_handler import DBConnectionHandler
 
 @pytest.mark.asyncio
 async def test_connection():
-    async with DBConnectionHandler() as session:
-        result = await session.execute(text("SELECT 1"))
-        assert result.scalar() == 1
+  async with DBConnectionHandler() as session:
+    result = await session.execute(text("SELECT 1"))
+    assert result.scalar() == 1
